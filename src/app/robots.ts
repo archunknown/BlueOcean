23db1319a61ac5d69e8136bec1af9cf5e90a7147
Adrian <adriantasayco99@gmail.com>
2025-12-15 15:39:12 -0500

    Correccion Robots y Sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://www.blueoceanparacastours.com/sitemap.xml',
+        // CORRECCIÓN: Eliminado 'www'
+        sitemap: 'https://blueoceanparacastours.com/sitemap.xml',
     };
 }
